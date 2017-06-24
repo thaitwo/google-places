aae97a4578c23329836936add25619dae2214363
thaitwo <tmtu90@gmail.com>
2017-06-23 23:51:38 -0700

    Add autocomplete and map

--- dev/app.js
+++ dev/app.js
@@ -4,43 +4,125 @@ import './scss/style.scss';
 
 class App {
   constructor() {
-    this.infowindow;
-    this.map;
-    this.sf = new google.maps.LatLng(37.7576793,-122.5076401);
-    this.pyrmont = {lat: 37.7847028, lng: -122.4001237};
+    // this.infowindow;
+    // this.map;
+    this.sf = new google.maps.LatLng(37.7831, -122.4039);
+    // this.pyrmont = {lat: 37.7847028, lng: -122.4001237};
+    this.card = document.getElementById('card');
 
-    this.request = {
-      location: this.pyrmont,
-      radius: 500,
-      type: ['restaurant']
-    };
+    // MAP
+    this.mapOptions = {
+      center: this.sf,
+      zoom: 12,
+    }
+    this.map = new google.maps.Map(document.getElementById('map'), this.mapOptions);
 
-    this.renderMap();
+    // AUTOCOMPLETE
+    this.$searchContainer = document.getElementById('search-ac');
+    this.acOptions = {
+      types: ['establishment']
+    }
 
-    this.defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
+    // this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(this.card);
 
-    this.input = document.getElementById('searchTextField');
-    this.options = {
-      bounds: this.defaultBounds,
-      types: ['geocode']
-    };
-
-    this.autocomplete = new google.maps.places.Autocomplete(this.input, this.options);
+    this.autocomplete = new google.maps.places.Autocomplete(this.$searchContainer,this.acOptions);
     this.autocomplete.bindTo('bounds', this.map);
+
+    // MARKER
+    this.markerOptions = {
+      position: this.sf
+    }
+    this.infoWindow = new google.maps.InfoWindow();
+    this.marker = new google.maps.Marker({
+      map: this.map
+    });
+    // this.marker.setMap(this.map);
+
+
+
+    google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
+      this.infoWindow.close();
+      const place = this.autocomplete.getPlace();
+      console.log(place);
+
+
+      if (place.geometry.viewport) {
+        this.map.fitBounds(place.geometry.viewport);
+      } else {
+        this.map.setCenter(place.geometry.location);
+        this.map.setZoom(17);
+      }
+
+
+      this.marker.setPosition(place.geometry.location);
+      this.infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+      this.infoWindow.open(this.map, this.marker);
+
+
+      google.maps.event.addListener(this.marker,'click', (e) => {
+
+        this.infoWindow.open(this.map, this.marker);
+
+      });
+    });
+
+
+
+
+    // INFO WINDOW
+    // this.infoWindowOptions = {
+    //   content: 'Moscone Is Here!'
+    // };
+
+    // this.infoWindow = new google.maps.InfoWindow(this.infoWindowOptions);
+
+    // google.maps.event.addListener(this.marker,'click', (e) => {
+
+    //   this.infoWindow.open(this.map, this.marker);
+
+    // });
+
+
+    // this.request = {
+    //   location: this.pyrmont,
+    //   radius: 500,
+    //   type: ['restaurant']
+    // };
+
+    // this.renderMap();
+
+    // this.input = document.getElementById('searchTextField');
+    // this.options = {
+    //   bounds: this.defaultBounds,
+    //   types: ['geocode']
+    // };
+
+
+
+    // this.autocomplete = new google.maps.places.Autocomplete(this.input);
+    // this.autocomplete.bindTo('bounds', this.map);
+
+    // this.infowindow = new google.maps.InfoWindow();
+
+    // this.defaultBounds = new google.maps.LatLngBounds(
+    //   new google.maps.LatLng(-33.8902, 151.1759),
+    //   new google.maps.LatLng(-33.8474, 151.2631));
+
+
   }
 
 
   renderMap() {
+    // Render map
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: this.pyrmont,
       zoom: 15
     });
 
-    this.infowindow = new google.maps.InfoWindow();
-    const service = new google.maps.places.PlacesService(this.map);
-    service.nearbySearch(this.request, this.callback.bind(this));
+    // Display pop-up window with place info
+    // this.infowindow = new google.maps.InfoWindow();
+    // const service = new google.maps.places.PlacesService(this.map);
+    // service.nearbySearch(this.request, this.callback.bind(this));
   }
 
 
@@ -59,15 +141,15 @@ class App {
   createMarker(place) {
     const that = this;
 
-    const placeLoc = place.geometry.location;
+    const placeLocation = place.geometry.location;
     const marker = new google.maps.Marker({
       map: this.map,
-      position: place.geometry.location
+      position: placeLocation
     });
 
     google.maps.event.addListener(marker, 'click', function() {
       that.infowindow.setContent(place.name);
-      that.infowindow.open(map, this);
+      that.infowindow.open(map, marker);
     });
   }
 }
--- public/js/app.compile.js
+++ public/js/app.compile.js
@@ -88,44 +88,119 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var App = function () {
   function App() {
+    var _this = this;
+
     _classCallCheck(this, App);
 
-    this.infowindow;
-    this.map;
-    this.sf = new google.maps.LatLng(37.7576793, -122.5076401);
-    this.pyrmont = { lat: 37.7847028, lng: -122.4001237 };
+    // this.infowindow;
+    // this.map;
+    this.sf = new google.maps.LatLng(37.7831, -122.4039);
+    // this.pyrmont = {lat: 37.7847028, lng: -122.4001237};
+    this.card = document.getElementById('card');
 
-    this.request = {
-      location: this.pyrmont,
-      radius: 500,
-      type: ['restaurant']
+    // MAP
+    this.mapOptions = {
+      center: this.sf,
+      zoom: 12
     };
+    this.map = new google.maps.Map(document.getElementById('map'), this.mapOptions);
 
-    this.renderMap();
+    // AUTOCOMPLETE
+    this.$searchContainer = document.getElementById('search-ac');
+    this.acOptions = {
+      types: ['establishment']
 
-    this.defaultBounds = new google.maps.LatLngBounds(new google.maps.LatLng(-33.8902, 151.1759), new google.maps.LatLng(-33.8474, 151.2631));
+      // this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(this.card);
 
-    this.input = document.getElementById('searchTextField');
-    this.options = {
-      bounds: this.defaultBounds,
-      types: ['geocode']
-    };
-
-    this.autocomplete = new google.maps.places.Autocomplete(this.input, this.options);
+    };this.autocomplete = new google.maps.places.Autocomplete(this.$searchContainer, this.acOptions);
     this.autocomplete.bindTo('bounds', this.map);
+
+    // MARKER
+    this.markerOptions = {
+      position: this.sf
+    };
+    this.infoWindow = new google.maps.InfoWindow();
+    this.marker = new google.maps.Marker({
+      map: this.map
+    });
+    // this.marker.setMap(this.map);
+
+
+    google.maps.event.addListener(this.autocomplete, 'place_changed', function () {
+      _this.infoWindow.close();
+      var place = _this.autocomplete.getPlace();
+      console.log(place);
+
+      if (place.geometry.viewport) {
+        _this.map.fitBounds(place.geometry.viewport);
+      } else {
+        _this.map.setCenter(place.geometry.location);
+        _this.map.setZoom(17);
+      }
+
+      _this.marker.setPosition(place.geometry.location);
+      _this.infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+      _this.infoWindow.open(_this.map, _this.marker);
+
+      google.maps.event.addListener(_this.marker, 'click', function (e) {
+
+        _this.infoWindow.open(_this.map, _this.marker);
+      });
+    });
+
+    // INFO WINDOW
+    // this.infoWindowOptions = {
+    //   content: 'Moscone Is Here!'
+    // };
+
+    // this.infoWindow = new google.maps.InfoWindow(this.infoWindowOptions);
+
+    // google.maps.event.addListener(this.marker,'click', (e) => {
+
+    //   this.infoWindow.open(this.map, this.marker);
+
+    // });
+
+
+    // this.request = {
+    //   location: this.pyrmont,
+    //   radius: 500,
+    //   type: ['restaurant']
+    // };
+
+    // this.renderMap();
+
+    // this.input = document.getElementById('searchTextField');
+    // this.options = {
+    //   bounds: this.defaultBounds,
+    //   types: ['geocode']
+    // };
+
+
+    // this.autocomplete = new google.maps.places.Autocomplete(this.input);
+    // this.autocomplete.bindTo('bounds', this.map);
+
+    // this.infowindow = new google.maps.InfoWindow();
+
+    // this.defaultBounds = new google.maps.LatLngBounds(
+    //   new google.maps.LatLng(-33.8902, 151.1759),
+    //   new google.maps.LatLng(-33.8474, 151.2631));
+
   }
 
   _createClass(App, [{
     key: 'renderMap',
     value: function renderMap() {
+      // Render map
       this.map = new google.maps.Map(document.getElementById('map'), {
         center: this.pyrmont,
         zoom: 15
       });
 
-      this.infowindow = new google.maps.InfoWindow();
-      var service = new google.maps.places.PlacesService(this.map);
-      service.nearbySearch(this.request, this.callback.bind(this));
+      // Display pop-up window with place info
+      // this.infowindow = new google.maps.InfoWindow();
+      // const service = new google.maps.places.PlacesService(this.map);
+      // service.nearbySearch(this.request, this.callback.bind(this));
     }
   }, {
     key: 'callback',
@@ -141,15 +216,15 @@ var App = function () {
     value: function createMarker(place) {
       var that = this;
 
-      var placeLoc = place.geometry.location;
+      var placeLocation = place.geometry.location;
       var marker = new google.maps.Marker({
         map: this.map,
-        position: place.geometry.location
+        position: placeLocation
       });
 
       google.maps.event.addListener(marker, 'click', function () {
         that.infowindow.setContent(place.name);
-        that.infowindow.open(map, this);
+        that.infowindow.open(map, marker);
       });
     }
   }]);
